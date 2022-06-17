549e75db1dcb82ee796e78ddad0a85365b0012cf
ArkZaslavskyi <95150973+ArkZaslavskyi@users.noreply.github.com>
2022-06-17 15:32:24 +0000

    Deploying to gh-pages from @ ArkZaslavskyi/goit-js-hw-09@3ee6a6bee511eef4c76a0b1f78439182cae83f80 🚀

---
+++ 03-promises.01697e77.js
@@ -0,0 +1,2 @@
+new Promise((function(n,e){var o=Math.random()>.5;setTimeout((function(){o&&n("Куку это resolve"),e("все пропало это reject")}),1e3)})).then((function(n){return console.log(n)})).catch((function(n){return console.log(n)}));
+//# sourceMappingURL=03-promises.01697e77.js.map
---
+++ 03-promises.8c3faac8.js
@@ -0,0 +1,2 @@
+new Promise(((e,o)=>{const t=Math.random()>.5;setTimeout((()=>{t&&e("Куку это resolve"),o("все пропало это reject")}),1e3)})).then((e=>console.log(e))).catch((e=>console.log(e)));
+//# sourceMappingURL=03-promises.8c3faac8.js.map
