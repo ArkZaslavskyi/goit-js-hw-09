4b32b75ea8a24998259f26ce1ec418ce265d9423
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-06-18 13:27:08 +0300

    all done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,13 +1,61 @@
-/*
+import {Notify} from 'notiflix';
+
+const formRef = document.querySelector('.form');
+
+formRef.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  
+  const { delay, step, amount } = e.target.elements;
+
+  let delay_count = Number(delay.value);
+
+  for (let i = 1; i <= amount.value; i += 1) {
+    createPromise(i, delay_count)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+       })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    
+    delay_count += Number(step.value);
+  };
+  
+  e.target.reset();
+};
+
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({position, delay})
+      } else {
+        reject({position, delay})
+      }
+    }, delay);
+     });
+
 }
-*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 /*
 const makeOrder = dish => {
@@ -80,6 +128,8 @@ function onFetchError(error) {
   console.log(error);
 }
 */
+
+/*
 const makePromise = () => {
   return new Promise((resolve, reject) => {
     const isPassed = Math.random() > 0.5;
@@ -97,3 +147,53 @@ const makePromise = () => {
 makePromise()
   .then(result => console.log(result))
   .catch(error => console.log(error));
+  */
+
+// const horses = [
+//   'Secretariat',
+//   'Eclipse',
+//   'West Australian',
+//   'Flying fox',
+//   'Seabisquit',
+// ];
+
+// console.log('%c Заезд начался, ставки не принимаются!',
+//             'color: brown; font-size: 14px;',
+// );
+
+// const promises = horses.map(run); 
+// console.log(promises);
+
+// Promise.race(promises).then(({ horse, time }) => {
+//   console.log(
+//     `%c Победил ${horse}, финишировав за ${time} времени`,
+//     'color: green; font-size: 14px;',
+//   );
+// });
+
+
+// // run('Mango')
+// //   .then(x => console.log(x));
+
+
+// function run(horse) {
+//   return new Promise((resolve) => {
+//     const time = getRandomTime(2000, 3500);
+
+//     setTimeout(() => {
+//       resolve({ horse, time });
+//     }, time);
+//   });
+// };
+
+
+// function getRandomTime(min, max) {
+//   return Math.floor(Math.random() * (max - min + 1) + min);
+// };
+
+
+
+// console.log('%c Заезд окончен, принимаются ставки',
+//             'color: blue; font-size: 14px;',
+// );
+
