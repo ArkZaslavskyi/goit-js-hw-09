48272bbc9094bd5fcc9d1d499b83c2cf2bb630e0
ArkZaslavskyi <ark.zaslavskyi@gmail.com>
2022-06-18 13:51:11 +0300

    update

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,5 +1,5 @@
-btnStartRef = document.querySelector('button[data-start]');
-btnStopRef = document.querySelector('button[data-stop]');
+btnStartRef = document.querySelector('button [data-start]');
+btnStopRef = document.querySelector('button [data-stop]');
 // 
 btnStopRef.setAttribute("disabled", true);
 // 
